e3b2e5938d6453b4241a456e8d113f8c2b642440
lijianyong <lijianyong@7275.com>
2016-03-14 20:25:55 +0800

    增加失败原因

--- Application/Crontab/Controller/CrontabController.class.php
+++ Application/Crontab/Controller/CrontabController.class.php
@@ -17,12 +17,13 @@ class CrontabController extends \Think\Controller{
 					$model->startTrans();
 					$res = $model->where("id={$value['id']}")->save(['onfile' => 1,'comission' => $comission]);
 					$res1 = $members->where("id={$value['members_id']}")->setInc('total_amount', $comission);
+					
 					if($res && $res1){
 						$model->commit();
 						$log_str .= '归档成功(id:' . $value['id'] . '	' . 'member_id:' . $value['members_id'] . '	' . "money:" . $comission . ")\r\n";
 					}else{
 						$model->rollback();
-						$log_str .= '归档失败(id:' . $value['id'] . '	' . 'member_id:' . $value['members_id'] . '	' . "money:" . $comission . ")({var_dump($res), var_dump($res1)})\r\n";
+						$log_str .= '归档失败(id:' . $value['id'] . '	' . 'member_id:' . $value['members_id'] . '	' . "money:" . $comission . ")($res, $res1})\r\n";
 					}
 				}
 			}
